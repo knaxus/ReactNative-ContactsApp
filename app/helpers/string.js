2a2670ab191b18b8530ec29595edf39c105a686b
cruxbreaker <geekankit318@gmail.com>
2018-06-05 14:49:03 +0530

    Added Helper Functions

---
+++ app/helpers/string.js
@@ -0,0 +1,13 @@
+// Helper Functions
+
+// Capitalizes first letter of string for better readability
+export const capitalizeFirstLetter = (string = '') => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  
+// Converts text to phone number
+  export const toPhoneNumber = (text = '') => {
+const modText = text.replace(/[^\d]/g, '');
+return modText.replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, '$1-$2-$3');
+};
+  
